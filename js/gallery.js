dcdccb507ac0063c360a1ca13a48e05318904156
Alex-Kun <alejandro.quiros@live.u-tad.com>
2019-04-19 23:19:40 +0200

    first commit

---
+++ js/gallery.js
@@ -0,0 +1,26 @@
+$(function(){
+  //set gallery
+  var pictures = [
+    "album/1.jpg",
+    "album/2.jpg",
+    "album/3.jpg",
+  ];
+
+  for (var i = 0; i < pictures.length; i++) {
+    var $node = "<div class='col-lg-3 col-12'><img class='pic' src="+pictures[i]+"></div>";
+    $('#here').append($node);
+  }
+  //set gallery END
+
+  $('.pic').click(function(){
+    $this = $(this),
+    $src = $this.attr('src');
+    $('.gallery .main').attr('src', $src);
+    $('.gallery').removeClass('d-none');
+    $('body').addClass('noscroll');
+  });
+  $('.gallery').click(function(){
+    $('.gallery').addClass('d-none');
+    $('body').removeClass('noscroll');
+  });
+});
