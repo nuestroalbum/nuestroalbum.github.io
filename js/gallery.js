4360cd55c6aeaa3e5eefa7a39ac1dc7a119f0b86
Alex-Kun <alejandro.quiros@live.u-tad.com>
2019-04-19 23:41:54 +0200

    a

--- js/gallery.js
+++ js/gallery.js
@@ -7,7 +7,7 @@ $(function(){
   ];
 
   for (var i = 0; i < pictures.length; i++) {
-    var $node = "<div class='col-lg-3 col-12'><img class='pic' src="+pictures[i]+"></div>";
+    var $node = "<div class='col-lg-3 col-6'><img class='pic' src="+pictures[i]+"></div>";
     $('#here').append($node);
   }
   //set gallery END
